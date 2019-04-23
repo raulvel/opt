2b1985980c5c464ce5183b083a59e7e5aa0fc539
Raul Velcherean <raul.velc@gmail.com>
2019-04-23 16:13:28 +0300

    script to check > error is decent w/o intensification

---
+++ test_script.m
@@ -0,0 +1,8 @@
+close all;
+point = [2 -1.5];
+goldstein_price(point)
+%plot3(point(1),point(2),goldstein_price(point),'*');
+%hold;
+pointts = sts(point)
+%goldstein_price(pointts)
+%plot3(pointts(1),pointts(2),goldstein_price(pointts),'*');
