6e9c788648c5ae8e8f23ba0ae0d3640a38b88f03
Raul Velcherean <raul.velc@gmail.com>
2019-04-23 13:07:58 +0300

    neighbour generation

---
+++ gen_crowns.m
@@ -0,0 +1,23 @@
+function points = gen_crowns(P, k )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+rup = 0.3;
+rdown = 0;
+
+r = (rup-rdown)*rand + rdown;
+rr = rand;
+x = r*cos(rr*2*pi) + P(1,1);
+y = r*sin(rr*2*pi) + P(1,2);
+points = [x, y];
+for i=2:k
+    %generate a random radius in crown interval
+    r = (rup-rdown)*rand + rdown;
+    %generate a random point on the circle w/ radius r
+    rr = rand;
+    x = r*cos(rr*2*pi) + P(i,1);
+    y = r*sin(rr*2*pi) + P(i,2);
+    points = [points; x, y];
+    rup = rup+0.3;
+    rdown = rdown + 0.3;
+end
+end
