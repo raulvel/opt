54cb9e52d7ed7ffa278d59e711a48fc907a50fd9
Raul Velcherean <raul.velc@gmail.com>
2019-04-23 15:58:47 +0300

    working?

--- sts.m
+++ sts.m
@@ -5,13 +5,14 @@ s = P;
 best_point = s;
 tl = s;
 stop = 0;
-while(stop>4)
+while(stop<4)
     %generate neighbours
     neigh = gen_crowns(s,4);
     %best neighbour
-    bn = neigh(1,:);
+    %bn = neigh(1,:);
+    bn = [3 3];
     for i=1:size(neigh,1)
-        if(~(is_tabu(neigh(i,:),tl))&&(goldstein_price(neigh(i,1),neigh(i,2)) < goldstein_price(bn(1),bn(2))))
+        if(~(is_tabu(neigh(i,:),tl))&&(goldstein_price([neigh(i,1),neigh(i,2)]) < goldstein_price([bn(1),bn(2)])))
             bn = neigh(i,:);
         end
     end
@@ -21,7 +22,7 @@ while(stop>4)
         stop = stop + 1;
     else
         s = bn;
-        if(goldstein_price(s(1),s(2)) < goldstein_price(best_point(1),best_point(2)))
+        if(goldstein_price([s(1),s(2)]) < goldstein_price([best_point(1),best_point(2)]))
             best_point = s;
         end
         tl = [tl; s];
